fd4054a482316c8f4c1ccdb41fd27fc72b7f4e76
matevosyan28 <matevosyan28karen@gmail.com>
2024-05-25 19:19:55 +0400

    created x functional

---
+++ app.js
@@ -0,0 +1,29 @@
+let arr = ['','','','','','','','','',]
+
+let div = document.getElementById('div')
+
+let xo = true
+
+let box = () =>{
+    for(let i = 1; i < arr.length;i++){
+        let block = document.createElement('div')
+        block.className = 'block'
+        div.appendChild(block)
+
+block.addEventListener('click', () => {
+    if(xo && arr[i] == ''){
+        arr[i] = 'x'
+        block.innerText = arr[i]
+
+    }
+    else if( !xo && arr[i] == ''){
+        arr[i] = 'o'
+        block.innerText = arr[i]
+    }
+    xo = !xo
+} )
+
+    }
+    
+}
+
